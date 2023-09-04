d41cd810ec62e68b9490a8a031fc83e1f100fbc2
Opeyemi-Omotayo <Opeyemi.Motayo1@gmail.com>
2023-09-04 20:58:24 +0100

    feat:modal

---
+++ src/components/modal/Modal.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Modal = () => {
+  return (
+      <div >
+        <h2>congrats</h2>
+        <p>You answered 5 questions correctly</p>
+        <button>
+          play again
+        </button>
+      </div>
+  );
+};
+
+export default Modal;
