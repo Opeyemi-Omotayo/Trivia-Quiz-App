dff6c1d39b22bd6f0b8432de00635fab90fb5b50
Opeyemi-Omotayo <Opeyemi.Motayo1@gmail.com>
2023-09-13 14:40:23 +0100

    UI: question component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Form from './components/form/Form';
 import Result from './components/result/Result';
+import Question from './components/questions/Question';
 
 function App() {
   return (
-    <Form />
+   // <Form />
     //<Result />
+    <Question />
   );
 }
 
--- src/components/questions/Question.tsx
+++ src/components/questions/Question.tsx
@@ -2,14 +2,19 @@ import React from 'react';
 
 const Question = () => {
   return (
-    <div>
-      <section>
-        <p>correct answers: 5</p>
+    <div className="h-[100vh] font-mono flex items-center justify-center bg-gradient-to-tr from-purple-700 via-purple-400 to-purple-200">
+      <section className="bg-white p-8 rounded-lg shadow-lg w-[70%]">
+        <div className='flex items-end justify-end   '>
+        <p className='text-green-500'>correct answers: 5</p>
+         </div>
         <article >
-          <h2>Questions</h2>
+          <h2 className='text-center font-bold text-2xl'>Questions</h2>
           <div ></div>
         </article>
-        <button>next questions</button>
+        <div className='flex items-end justify-end   '>
+        <button type="submit" className=" mt-6 w-[35%] text-lg text-white bg-purple-700 p-3  rounded-md shadow-md">next questions</button>        
+         </div>
+
       </section>
     </div>
   )
